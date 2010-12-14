7a551d017aa0860a0a2928175bb58a215a24fd64
schaermu <schaermu@gmail.com>
2010-12-14 13:58:08 +0100

    improved DataContracts.tt (parsing EnumMembers as pre-defined variables) DataContracts can now be rendered as a single file containing all contracts (used for require_once)

--- WcfT4Client.PhpGenerator/AdressDto.php
+++ WcfT4Client.PhpGenerator/AdressDto.php
@@ -1,27 +1,29 @@
 <?php
 
+// START OF CLASS AdressDto
 class AdressDto {
-
-	/**
+	
+  /**
 	 * @var String
    */
 	public $Street;
-
-	/**
+	
+  /**
 	 * @var String
    */
 	public $PostalCode;
-
-	/**
+	
+  /**
 	 * @var String
    */
 	public $City;
-
-	/**
+	
+  /**
 	 * @var String
    */
 	public $Country;
-
 }
+// END OF CLASS AdressDto
 
 ?>
+		
--- WcfT4Client.PhpGenerator/CustomerDto.php
+++ WcfT4Client.PhpGenerator/CustomerDto.php
@@ -1,27 +1,29 @@
 <?php
 
+// START OF CLASS CustomerDto
 class CustomerDto {
-
-	/**
+	
+  /**
 	 * @var Int64
    */
 	public $Id;
-
-	/**
+	
+  /**
 	 * @var AdressDto
    */
 	public $Adress;
-
-	/**
+	
+  /**
 	 * @var String
    */
 	public $Name;
-
-	/**
+	
+  /**
 	 * @var DateTime
    */
 	public $Dob;
-
 }
+// END OF CLASS CustomerDto
 
 ?>
+		
